5b20b47dfd00b9a0c7c4e69f6436a11484ee70d2
Naim Miah <mmiah@fordham.edu>
2023-08-23 15:23:42 -0400

    almost done

--- practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
+++ practice-for-ch-js-ajax-twitter-long-practice-main/app/javascript/util/api.js
@@ -3,8 +3,25 @@ const csrfToken = document.querySelector("meta[name=csrf-token]").content;
 async function customFetch(url, options = {}) {
   options.headers = {
     // Your code here
-    ...options.headers
+    ...options.headers,
+    "Content-Type": "application/json",
+    "X-CSRF-Token": csrfToken,
   };
 
   return await fetch(url, options);
+}
+
+export async function followUser(userId) {
+  // Your code here
+
+  return customFetch(`/users/${userId}/follow`, {
+    method: "POST",
+  });
+}
+
+export async function unfollowUser(userId) {
+  // Your code here
+  return customFetch(`/users/${userId}/follow`, {
+    method: "DELETE",
+  });
 }
